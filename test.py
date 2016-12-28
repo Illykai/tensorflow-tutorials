4d7fa9feb8729e49afd07d27cf99f005d86dbf2f
Owen Macindoe <owenm@arena.net>
2016-12-28 11:22:14 -0800

    Tinkering with debugging.

--- test.py
+++ test.py
@@ -2,10 +2,12 @@
 
 def main():
     """Testing this thing"""
-
     print("This works")
+    for foo in range(0,10):
+        print(foo)
 
 if __name__ == "__main__":
     # Testing Git stuff
+    # Repository url: https://github.com/Illykai/tensorflow-tutorials.git
     main()
     
